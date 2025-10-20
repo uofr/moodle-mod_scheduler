5bf9b302559b5d8f80601a3766fbe31a45e2051c
John Lane <46030891+JohnOLane@users.noreply.github.com>
2025-10-20 15:16:19 -0600

    Freeze checkbox in appointment form based on scheduler permissions.

--- appointmentforms.php
+++ appointmentforms.php
@@ -92,7 +92,9 @@ class scheduler_editappointment_form extends moodleform {
 
         //CHANGE add if time is <=24 then show
         $mform->addElement('checkbox', 'attended', get_string('attended', 'scheduler'));
-        $mform->freeze('attended');
+        if (!$this->permissions->can_edit_attended($this->appointment)) {
+            $mform->freeze('attended');
+        }
 
         // Grade.
         if ($scheduler->uses_grades()) {
