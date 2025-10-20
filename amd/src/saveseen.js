66c2f4a747d8d2c13281b28ed8bcf907d4b66799
John Lane <46030891+JohnOLane@users.noreply.github.com>
2025-10-20 15:14:12 -0600

    Handle custom checkboxes in js.

--- amd/src/saveseen.js
+++ amd/src/saveseen.js
@@ -1,6 +1,15 @@
 
-export const SELECTORS = {CHECKBOXES: 'table#slotmanager form.studentselectform input.studentselect'};
+export const SELECTORS = {
+    CHECKBOXES: 'table#slotmanager form.studentselectform input.attendancebox',
+};
+
 export const MOD = {};
+
+const ACTIONS = [
+    'saveseen',
+    'absentpaid',
+    'absentschedule'
+];
 
 /**
  * Save the "seen" status.
@@ -9,11 +18,12 @@ export const MOD = {};
  * @param {Number} appid the id of the relevant appointment
  * @param {Boolean} newseen
  * @param {Object} spinner The spinner icon shown while saving
+ * @param {String} action The action being taken (saveseen, absentpaid, absentschedule)
  */
-export const save_status = (cmid, appid, newseen, spinner, ) => {
+export const save_status = (cmid, appid, newseen, spinner, action) => {
     const url = M.cfg.wwwroot + '/mod/scheduler/ajax.php';
     // The request paramaters.
-    const params = 'action=saveseen&id='+cmid+'&appointmentid='+appid+'&seen='+newseen+'&sesskey='+M.cfg.sesskey;
+    const params = `action=${action}&id=${cmid}&appointmentid=${appid}&seen=${newseen}&sesskey=${M.cfg.sesskey}`;
     const xhr = new XMLHttpRequest();
 
     // 5 seconds of timeout.
@@ -58,13 +68,18 @@ export const save_status = (cmid, appid, newseen, spinner, ) => {
 export const init = (cmid) => {
     document.querySelectorAll(SELECTORS.CHECKBOXES).forEach(function(box) {
         box.addEventListener('change', function() {
-            var div = document.createElement('div');
-            var span = document.createElement('span');
+            const action = box.getAttribute('data-action');
+            const div = document.createElement('div');
+            const span = document.createElement('span');
+            const otherActions = ACTIONS.filter(ac => ac != action);
+            otherActions.forEach(ac => {
+                box.closest('div').querySelector(`input[data-action=${ac}]`).checked = false;
+            });
             div.classList.add('spinner-border', 'spinner-border-sm');
             span.classList.add('sr-only');
             div.appendChild(span);
             box.closest('div').appendChild(div);
-            save_status(cmid, box.value, box.checked, div);
+            save_status(cmid, box.value, box.checked, div, action, box);
         });
     });
 };
