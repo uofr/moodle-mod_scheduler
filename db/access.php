17424e250a6e963a6cdb5cd443f34363cd2d10ad
John Lane <46030891+JohnOLane@users.noreply.github.com>
2025-10-20 15:18:00 -0600

    Allow unlimitedeting capability for managers.

--- db/access.php
+++ db/access.php
@@ -132,8 +132,8 @@ $capabilities = array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_MODULE,
         'archetypes' => array(
-            'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
         )
     ),
 
