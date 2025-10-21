206e0e6adb7aee6a2f2cd34f280f320c866bbbfa
John Lane <46030891+JohnOLane@users.noreply.github.com>
2025-10-21 08:50:19 -0600

    Revert "Hide attended checkbox on student view page. Make sure attended is set in formdata so nothing is reset."
    This reverts commit 47956ff040431320b2850fe695c09d88a05b3430.

--- appointmentforms.php
+++ appointmentforms.php
@@ -91,10 +91,10 @@ class scheduler_editappointment_form extends moodleform {
         // Seen tickbox.
 
         //CHANGE add if time is <=24 then show
-        // $mform->addElement('checkbox', 'attended', get_string('attended', 'scheduler'));
-        // if (!$this->permissions->can_edit_attended($this->appointment)) {
-        //     $mform->freeze('attended');
-        // }
+        $mform->addElement('checkbox', 'attended', get_string('attended', 'scheduler'));
+        if (!$this->permissions->can_edit_attended($this->appointment)) {
+            $mform->freeze('attended');
+        }
 
         // Grade.
         if ($scheduler->uses_grades()) {
