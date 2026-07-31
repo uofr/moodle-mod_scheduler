c105373792997bbd5306b99f4c493a07a05bc8de
lenaherf <lherfeld@uni-muenster.de>
2026-07-31 15:16:10 +0200

    fix alphabetical order of interfaces

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -25,13 +25,16 @@
 namespace mod_scheduler\privacy;
 
 use core_privacy\local\metadata\collection;
+use core_privacy\local\metadata\provider as metadata_provider;
 use core_privacy\local\request\approved_contextlist;
 use core_privacy\local\request\approved_userlist;
-use core_privacy\local\request\contextlist;
-use core_privacy\local\request\userlist;
-use core_privacy\local\request\helper;
 use core_privacy\local\request\content_writer;
+use core_privacy\local\request\contextlist;
+use \core_privacy\local\request\core_userlist_provider;
+use core_privacy\local\request\helper;
+use core_privacy\local\request\plugin\provider as plugin_provider;
 use core_privacy\local\request\transform;
+use core_privacy\local\request\userlist;
 use core_privacy\local\request\writer;
 
 /**
@@ -43,9 +46,9 @@ use core_privacy\local\request\writer;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\core_userlist_provider,
-    \core_privacy\local\request\plugin\provider {
+    core_userlist_provider,
+    metadata_provider,
+    plugin_provider {
     /** @var mixed */
     private static $renderer;
 
